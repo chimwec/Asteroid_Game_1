93cbd9ff057be9c1360a4a9863331ca88c70ea2c
Chim chikwakwa <chimwec98@gmail.com>
2024-08-23 18:59:35 +0200

    this one

--- main.py
+++ main.py
@@ -18,7 +18,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-     This repo is for a game that i want to practice develop, i have done few but this will be more advanced one yet. 
- I want it to demonstrate my versitility
